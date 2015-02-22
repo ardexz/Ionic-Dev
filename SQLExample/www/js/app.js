68186ef31c0043aee37d722f694c1c2a3553127d
Joel Mathys <joelmathys@joels-mac-mini.home>
2015-02-22 13:03:53 +0100

    init

---
+++ SQLExample/www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+
+
+var db = null;
+ 
+var example = angular.module('starter', ['ionic', 'ngCordova'])
+    .run(function($ionicPlatform, $cordovaSQLite) {
+        $ionicPlatform.ready(function() {
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+            db = $cordovaSQLite.openDB({ name: "my.db" });
+            $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+            console.log("db should now be either opened or created");
+
+        });
+    });
+
+
+example.controller("ExampleController", function($scope, $cordovaSQLite) {
+ 
+    $scope.insert = function(firstname, lastname) {
+        console.log("call insert function with: "+firstname+lastname);
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
+            console.log("INSERT ID -> " + res.insertId);
+        }, function (err) {
+            console.error(err);
+        });
+    }
+ 
+    $scope.select = function(lastname) {
+        console.log("call select function with: "+ lastname);
+        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
+            if(res.rows.length > 0) {
+                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+            } else {
+                console.log("No results found");
+            }
+        }, function (err) {
+            console.error(err);
+        });
+    }
+ 
+});
